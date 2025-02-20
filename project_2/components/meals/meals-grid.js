b582d6b784c9274cdcad6a7924fe406d8c156cea
Ziad Elsayed <ZiadElsayedAsr@gmail.com>
2025-02-20 02:16:30 +0200

    Adding meals Page, meals grid component and meals item component

---
+++ project_2/components/meals/meals-grid.js
@@ -0,0 +1,16 @@
+import MealsItem from "./meals-item";
+import styles from "./meals-grid.module.css";
+
+const MealsGrid = ({ meals }) => {
+  return (
+    <nav className={styles.meals}>
+      {meals.map((meal) => {
+        <li key={meal.id}>
+          <MealsItem {...meal} />
+        </li>;
+      })}
+    </nav>
+  );
+};
+
+export default MealsGrid;
