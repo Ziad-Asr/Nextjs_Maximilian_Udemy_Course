ab072e2729f834a12b9cd7e4bae3101fff2bc057
Ziad Elsayed <ZiadElsayedAsr@gmail.com>
2025-02-22 10:51:37 +0200

    configure working with cloudinary, coudinary integration and enhacing images performance

--- project_6_optimization/next.config.mjs
+++ project_6_optimization/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [{ hostname: "res.cludinary.com" }],
+  },
+};
 
 export default nextConfig;
